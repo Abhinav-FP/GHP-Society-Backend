5ae8099b5469dd2b31c9b9f0d369dfb73c0a3667
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2025-05-02 17:13:52 +0530

    Ghp Backedn

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ dotenv.config();
 
 const express = require("express");
 const app = express();
+const serverless = require("serverless-http");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
+
 const corsOptions = {
-  origin: "*", // Allowed origins
+  origin: "*",
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: '*', // Allow all headers
+  allowedHeaders: '*',
   credentials: true,
-  optionsSuccessStatus: 200, // for legacy browsers
-}
+  optionsSuccessStatus: 200,
+};
 
 app.use(cors(corsOptions));
 app.use(express.json({ limit: '2000mb' }));
 app.use(express.urlencoded({ extended: true, limit: "2000mb" }));
+
 
 const PORT = process.env.REACT_APP_SERVER_DOMAIN || 5100;
 
@@ -179,3 +183,6 @@ app.get("/", (req, res) => {
 
 const server = app.listen(PORT, () => console.log("Server is running at port : " + PORT));
 server.timeout = 360000; // 6 minutes
+module.exports = app;
+module.exports.handler = serverless(app);
+
